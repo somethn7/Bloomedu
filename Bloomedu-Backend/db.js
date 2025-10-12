c60e93635aa06ead6b730bee625e9ba3cd3b8282
somethn7 <gozdeertas21080@gmail.com>
2025-10-12 22:31:51 +0300

    Update db.js and environment settings for Supabase and deployment

--- Bloomedu-Backend/db.js
+++ Bloomedu-Backend/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',       // PostgreSQL kullanıcı adın
-  host: 'localhost',
-  database: 'bloomedu',   // senin veritabanı adı
-  password: 'Project123!!',  // PostgreSQL şifren
-  port: 5432,             // genelde 5432 olur
+  user: 'postgres.xxjldvlmcyhhxkeivitw',  // connection string'deki kullanıcı adı TAM olarak bu
+  host: 'aws-1-eu-north-1.pooler.supabase.com', // yeni host
+  database: 'postgres', // varsayılan database ismi
+  password: 'Project123!!', // senin belirlediğin şifre
+  port: 6543, // dikkat! 5432 değil 6543 olacak
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
